76a721edf4c10b1ac876642f8fc8cb39ae8d5dc3
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2017-09-10 18:02:24 -0400

    book detail

--- routes/books.js
+++ routes/books.js
@@ -11,16 +11,14 @@ let filter;
 
 
 let bookQuery;
+// use loanQuery for book detail
+let loanQuery;
 
 // GET all books
 router.get('/', (req, res, next) => {
-    console.log("these are the books");
     // grab all the books
-    console.log(req.query);
-    console.log(typeof req.query);
     if (req.query.filter === undefined) {
         bookQuery = Book.findAll({ order: [["first_published", "DESC"]] }).then((books) => {
-            console.log(books);
             res.render('all_books', {
                 books: books,
                 title: 'Books'
@@ -32,7 +30,6 @@ router.get('/', (req, res, next) => {
 
     // filter for overdue books
     // SELECT * FROM books, loans WHERE  books.id=loans.book_id AND  loans.returned_on IS NULL AND loans.return_by <= "2017-08-17"
-    console.log(moment().format("YYYY-MM-DD"))
     if (req.query.filter === 'overdue') {
         bookQuery = Book.findAll({
             include: [{
@@ -80,19 +77,60 @@ router.get('/new', (req, res, next) => {
 
 // POST a book to the database
 router.post('/new', (req, res, next) => {
-    Book.create(req.body).then((newBook) => {
-        res.redirect('/books' + newBook.id);
-        res.render('all_books', {
-            newBook: newBook
+    let errorMessages = [];
+
+    const title = req.body.title;
+    const author = req.body.author;
+    const genre = req.body.genre;
+    const firstPublished = req.body.first_published;
+    if (!title) {
+        errorMessages.push("Please enter a title!");
+    }
+
+    if (!author) {
+        errorMessages.push("Please enter an author!");
+    }
+
+    if (!genre) {
+        errorMessages.push("Please enter a genre!");
+    }
+
+    if (errorMessages.length === 0) {
+        Book.create(req.body).then((newBook) => {
+            res.redirect('/books');
+        })
+    } else {
+        res.render('new_book', {
+            errorMessages: errorMessages,
+            title: title,
+            author: author,
+            genre: genre,
+            firstPublished: firstPublished
         });
-    })
+    }
 });
 
-// // POST create new book
-// router.post('/', (req, res, next) => {
-//     Book.create(req.body).then((book) => {
-//         res.redirect('all_books' + book.id);
-//     });
-// });
+// GET the book detail page
+router.get('/:id', (req, res, next) => {
+    loanQuery = Loan.findAll({ order: [["loaned_on", "DESC"]] }).then((loans) => {
+        include: [
+            {model: Patron,},
+            {model: Book}
+        ],
+        res.render('book_detail', {
+            loans: loans,
+            // title: loans.Book.title,
+            // author: loans.Book.author,
+            // genre: loans.Book.genre,
+            // first_published: loans.Book.first_published,
+            // patronFirstName: loans.Patron.first_name,
+            // patronLastName: loans.Patron.last_name,
+            // loanedOn: loans.loaned_on,
+            // return_by: loans.return_by,
+            // returned_on: loans.returned_on
+        
+        });
+    });
+});
 
 module.exports = router;
