0cc3fe8252c88d2300a6ba7a045eb77f0c0712fd
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2017-09-15 00:44:50 -0400

    finish book detail

--- routes/books.js
+++ routes/books.js
@@ -9,16 +9,11 @@ const Patron = require('../models').Patron;
 
 let filter;
 
-
-let bookQuery;
-// use loanQuery for book detail
-let loanQuery;
-
 // GET all books
 router.get('/', (req, res, next) => {
     // grab all the books
     if (req.query.filter === undefined) {
-        bookQuery = Book.findAll({ order: [["first_published", "DESC"]] }).then((books) => {
+        Book.findAll({ order: [["first_published", "DESC"]] }).then((books) => {
             res.render('all_books', {
                 books: books,
                 title: 'Books'
@@ -31,7 +26,7 @@ router.get('/', (req, res, next) => {
     // filter for overdue books
     // SELECT * FROM books, loans WHERE  books.id=loans.book_id AND  loans.returned_on IS NULL AND loans.return_by <= "2017-08-17"
     if (req.query.filter === 'overdue') {
-        bookQuery = Book.findAll({
+        Book.findAll({
             include: [{
                 model: Loan,
                 where: {
@@ -53,7 +48,7 @@ router.get('/', (req, res, next) => {
     // filter for checked out books
     // SELECT * FROM books, loans WHERE books.id=loans.book_id AND loans.returned_on IS NULL
     if (req.query.filter === 'checked_out') {
-        bookQuery = Book.findAll({
+        Book.findAll({
             include: [{
                 model: Loan,
                 where: {
@@ -112,27 +107,67 @@ router.post('/new', (req, res, next) => {
 
 // GET the book detail page
 router.get('/:id', (req, res, next) => {
-    bookQuery = Book.findAll({
-        include: [
-            { model: Loan }
-        ],
-        order: [["title", "DESC"]]
-    }).then((books) => {
-        console.log(books);
-        res.render('book_detail', {
-            books: books,
-            // title: loans.Book.title,
-            // author: loans.Book.author,
-            // genre: loans.Book.genre,
-            // first_published: loans.Book.first_published,
-            // patronFirstName: loans.Patron.first_name,
-            // patronLastName: loans.Patron.last_name,
-            // loanedOn: loans.loaned_on,
-            // return_by: loans.return_by,
-            // returned_on: loans.returned_on
+    const getBook = Book.findOne({
+        where: [
+            { id: req.params.id }
+        ]
+    });
 
+    const getLoans = Loan.findAll({
+        where: [
+            { book_id: req.params.id }
+        ],
+        include: [{
+            model: Patron
+        }],
+    });
+
+    Promise.all([getBook, getLoans]).then(results => {
+        res.render('book_detail', {
+            book: results[0],
+            loans: results[1]
         });
     });
 });
+
+// UPDATE the book detail page
+router.post('/:id/update', (req, res, next) => {
+    let errorMessages = [];
+
+    const title = req.body.title;
+    const author = req.body.author;
+    const genre = req.body.genre;
+    const firstPublished = req.body.first_published;
+    if (!title) {
+        errorMessages.push("Please enter a title!");
+    }
+
+    if (!author) {
+        errorMessages.push("Please enter an author!");
+    }
+
+    if (!genre) {
+        errorMessages.push("Please enter a genre!");
+    }
+    console.log('@@@@@@@@@@@@@@@@@@');
+    console.log(req.body);
+    console.log(errorMessages);
+
+    if (errorMessages.length === 0) {
+        Book.update(req.body, { where: [{ id: req.params.id }] }).then((newBook) => {
+            res.redirect('/books');
+        })
+    }
+});
+
+// else {
+//     res.render('book_detail', {
+//         errorMessages: errorMessages,
+//         title: title,
+//         author: author,
+//         genre: genre,
+//         firstPublished: firstPublished
+//     });
+// }
 
 module.exports = router;
